453f441a88b7fea2dfc6126185c618b455d91b07
Parul <87116013+Cyber-Cat15@users.noreply.github.com>
2022-10-01 00:55:19 +0530

    Update third.c

--- third.c
+++ third.c
@@ -1,8 +1,10 @@
 //this is c code for input and output
 #include<stdio.h>
+#include<conio.h>
 main()
 {
   int age;
+  clrscr();
   printf("Enter your age:");
   scanf("%d",&age);
   printf("Your age is: %d",age);
